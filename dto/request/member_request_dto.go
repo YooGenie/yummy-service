f4dc7219cd0925b163796b5dc15dd27888fc941a
ysun7 <ysun7@beautifulstore.org>
2022-07-06 23:04:02 +0900

    #27 멤버등록할 때 validate 설정하기

--- dto/request/member_request_dto.go
+++ dto/request/member_request_dto.go
@@ -1,11 +1,27 @@
 package dto
 
+import (
+	"github.com/labstack/echo/v4"
+	log "github.com/sirupsen/logrus"
+)
+
 type MemberCreate struct {
-	Email    string `json:"email" validate:"lte=50,required"`
-	Password string `json:"password" validate:"lte=100,required"`
+	Email    string `json:"email" validate:"email,lte=50,required"`
+	Password string `json:"password" validate:"gte=6,lte=50,required"`
 	Name     string `json:"name" validate:"lte=50,required"`
-	Mobile   string `json:"mobile" validate:"required"`
+	Mobile   string `json:"mobile" validate:"mobile,required"`
 }
+
+func (v MemberCreate) Validate(ctx echo.Context) error {
+	log.Traceln("")
+
+	if err := ctx.Validate(v); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 
 type SearchMemberQueryParams struct {
 }
